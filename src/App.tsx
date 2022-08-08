9f9e5a7b073a95181056f3fb78fe77beed796523
xongjaemin <woals99@gmail.com>
2022-08-08 10:11:19 +0900

    add: 상세페이지

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/detail" element={<Detail/>} />
+        <Route path="/detail/:id" element={<Detail/>} />
       </Routes>
     </>
 
--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,10 +1,72 @@
 /** @jsxImportSource @emotion/react */
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import dotenv from "dotenv"
+import { useParams } from 'react-router-dom';
 import { css } from '@emotion/react';
+import axios from 'axios';
+import snsIcon from '../../assets/detail/sns_icon.png'
+import siteIcon from '../../assets/detail/site_icon.png'
 
 const Detail = () => {
 
-  const [recommend, setRecommend] = useState(['','','','','']);
+  let {id} = useParams();
+
+  const [clubInfo, setClubInfo] = useState(
+    {
+     club: {
+      activityDay: "",
+      applyUrl: null,
+      clubId: 0,
+      competition: "",
+      deadline: null,
+      introduction: null,
+      location: "",
+      logoUrl: "",
+      mainCategory: "",
+      membershipFee: null,
+      name: "",
+      online: 3,
+      period: "",
+      personnel: null,
+      recruiting: false,
+      reviews: [''],
+      selectionProcess: "",
+      siteAddress: "",
+      snsAddress: "",
+      subCategory: [],
+      target: "",
+      uniqueness: [''],
+      __v: 0,
+      _id: "",
+     },
+     recommendClub: [
+      {
+        logoUrl: "",
+        name: "",
+        _id: "",
+      }
+     ]
+      
+    }
+  );
+
+  const fetchData = async() => {
+    try{
+      const response = await axios.get(`${process.env.REACT_APP_BASE_URL}${process.env.REACT_APP_GET_DETAIL}${id}`);
+      console.log(response.data);
+      setClubInfo(response.data);
+    } catch(e){
+      console.log(e);
+    }
+    
+    console.log(clubInfo)
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  if (!clubInfo) return null;
 
   return (
     <div style={{overflow: 'hidden'}}>
@@ -12,33 +74,49 @@ const Detail = () => {
         <div css={summaryContainer}>
           <div css={title}>
             <div css={imageDiv}>
+                <img src={clubInfo.club.logoUrl} style={{width: '61px', height: '61px', borderRadius: '70%'}}/>
             </div>
             <div css={nameDiv}>
-              동아리이름
+              {clubInfo.club.name}
             </div>
           </div>
           <table css={summaryTable} style={{overflow: 'hidden'}}>
               <tbody>
                 <tr>
-                  <td css={categoryTD}>분야</td><td css={dataTD}></td>
+                  <td css={categoryTD}>분야</td><td css={dataTD}>{clubInfo.club.mainCategory}</td>
                 </tr>
                 <tr>
-                  <td css={categoryTD}>모집 여부</td><td css={dataTD}></td>
+                  <td css={categoryTD}>모집 여부</td>
+                  <td css={dataTD}>
+                    {clubInfo.club.recruiting
+                      ? <div>모집중</div>
+                      : <div>마감</div>
+                    }
+                  </td>
                 </tr>
                 <tr>
-                  <td css={categoryTD}>활동 기간</td><td css={dataTD}></td>
+                  <td css={categoryTD}>활동 기간</td><td css={dataTD}>{clubInfo.club.period}</td>
                 </tr>
                 <tr>
-                  <td css={categoryTD}>정기 요일</td><td css={dataTD}></td>
+                  <td css={categoryTD}>정기 요일</td><td css={dataTD}>{clubInfo.club.activityDay}</td>
                 </tr>
                 <tr>
-                  <td css={categoryTD}>회비</td><td css={dataTD}></td>
+                  <td css={categoryTD}>회비</td><td css={dataTD}>{clubInfo.club.membershipFee}</td>
                 </tr>
                 <tr>
-                  <td css={categoryTD}>선발 절차</td><td css={dataTD}></td>
+                  <td css={categoryTD}>선발 절차</td><td css={dataTD}>{clubInfo.club.selectionProcess}</td>
                 </tr>
                 <tr>
-                  <td style={{borderRight: '1.5px solid #DBDBDB', textAlign:'center', background: '#F8F8F8'}}>활동 방법</td><td style={{textAlign: 'center'}}></td>
+                  <td style={{borderRight: '1.5px solid #DBDBDB', textAlign:'center', background: '#F8F8F8'}}>활동 방법</td>
+                  <td style={{textAlign: 'center'}}>
+                    {
+                      clubInfo.club.online == 1
+                      ? <div>온라인</div>
+                      : clubInfo.club.online == 2
+                        ? <div>오프라인</div>
+                        : <div>온/오프라인</div>
+                    }
+                  </td>
                 </tr>
               </tbody>
           </table>
@@ -49,35 +127,58 @@ const Detail = () => {
                 어떤 동아리일까 ?
             </span>
             <div css={detailDescription}>
-              이런 이런 동아리다
+              {clubInfo.club.introduction}
             </div>
 
             <span css={detailTitle}>
-              에 대해 더 알아보자면..
+              {clubInfo.club.name}에 대해 더 알아보자면..
             </span>
             <div css={detailDescription}>
-              ㅇㄴㄹㅇ
+              {
+                clubInfo.club.uniqueness.map((a) => {
+                  return(
+                    <div style={{marginBottom: '10px'}}>
+                      - {a}
+                    </div>
+                  )
+                })
+              }
             </div>
 
             <span css={detailTitle}>
               어떤 사람들이 모인 곳일까 ?
             </span>
             <div css={detailDescription}>
-              ㅇㄴㄹㅇ
+              {clubInfo.club.target}이라면 지원 가능하며, 정원은 {clubInfo.club.personnel}입니다. 경쟁률은 {clubInfo.club.competition}입니다.
             </div>
 
             <span css={detailTitle}>
-              에 대해 더 알고 싶다면 ?
+              {clubInfo.club.name}에 대해 더 알고 싶다면 ?
             </span>
             <div css={detailDescription}>
-              ㄴㄹㅇㄴ
+              <div style={{display: 'flex', marginBottom: '19px'}}>
+                <img src={siteIcon} style={{width: '19px', height: '19px', marginRight: '16px'}}/>
+                <a href={clubInfo.club.snsAddress} style={{color: '#241E19'}}>{clubInfo.club.snsAddress}</a>
+              </div>
+              <div style={{display: 'flex'}}>
+                <img src={snsIcon} style={{width: '19px', height: '19px', marginRight: '16px'}}/>
+                <a href={clubInfo.club.siteAddress} style={{color: '#241E19'}}>{clubInfo.club.siteAddress}</a>
+              </div>
             </div>
 
             <span css={detailTitle}>
               실제 활동했던 사람들의 후기가 궁금하다면 ?
             </span>
             <div css={detailDescription}>
-              ㅇㄹㅇㄴ
+              {
+                clubInfo.club.reviews.map((a) => {
+                  return(
+                    <div style={{marginBottom: '10px'}}>
+                      <a href={a} style={{color: '#241E19'}}>{a}</a>
+                    </div>
+                  )
+                })
+              }
             </div>
 
             <span css={detailTitle}>
@@ -85,13 +186,14 @@ const Detail = () => {
             </span>
             <div css={detailDescription} style={{display: 'flex'}}>
               {
-                recommend.map((a) => {
+                clubInfo.recommendClub.map((a) => {
                   return(
                     <div style={{textAlign: 'center', marginRight: '33px'}}>
                       <div css={recommendImgContainer}>
+                        <img src={a.logoUrl} style={{width: '132px', height: '132px', borderRadius: '70%'}}/>
                       </div>
                       <div css={recommendTitle}>
-                        동아리이름
+                        {a.name}
                       </div>
                     </div>
                   )
@@ -125,6 +227,10 @@ const imageDiv = css`
   height: 61px;
   border-radius: 70%;
   border: 1.5px solid #DBDBDB;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const nameDiv = css`
@@ -222,6 +328,10 @@ const recommendImgContainer = css`
   height: 132px;
   border-radius: 70%;
   border: 1.5px solid #DBDBDB;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const recommendTitle = css`
